73e61aa6f306d4c671232ebf53bd1609b0d4cb36
CTurt <cturt@users.noreply.github.com>
2015-07-28 20:00:31 +0100

    Misc fixes

--- include/memory.h
+++ include/memory.h
@@ -3,7 +3,7 @@
 struct memoryRegionInfo {
 	void *base; // 0x0
 	void *end; // 0x8
-	uint64_t flags; // 0x16
+	unsigned int flags; // 0x16
 };
 
 int getMemoryInfo(void *address, struct memoryRegionInfo *destination);
