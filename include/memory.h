04cd12c2c6b663e233d45e397de2b1e3790ead2a
CTurt <cturt@users.noreply.github.com>
2015-07-28 21:48:12 +0100

    More memory info

--- include/memory.h
+++ include/memory.h
@@ -6,4 +6,12 @@ struct memoryRegionInfo {
 	unsigned int flags; // 0x16
 };
 
+struct otherMemoryRegionInfo {
+	void *base; // 0x0
+	void *end; // 0x8
+	char unknown[0xa]; // 0x16
+	char name[32]; // 0x20
+};
+
 int getMemoryInfo(void *address, struct memoryRegionInfo *destination);
+int getOtherMemoryInfo(void *address, struct otherMemoryRegionInfo *destination);
--- source/memory.c
+++ source/memory.c
@@ -5,3 +5,7 @@
 int getMemoryInfo(void *address, struct memoryRegionInfo *destination) {
 	return syscall(547, address, destination);
 }
+
+int getOtherMemoryInfo(void *address, struct otherMemoryRegionInfo *destination) {
+	return syscall(572, address, 0, destination, sizeof(struct otherMemoryRegionInfo));
+}
