ed87c801d19e6772658ce9ecbe587a9c35039641
CTurt <cturt@users.noreply.github.com>
2015-07-28 17:55:54 +0100

    Another unknown resource

--- include/unknown.h
+++ include/unknown.h
@@ -2,3 +2,6 @@
 
 int unknownResourceCreate(const char *name);
 int unknownResourceDestroy(int unknownResource);
+
+int unknownResource2Create(const char *name);
+int unknownResource2Destroy(int unknownResource);
