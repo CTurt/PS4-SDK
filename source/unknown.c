3abe07b758b6f2ff1fe5e98c76cf1c7cba88357e
CTurt <ertl.chris@gmail.com>
2015-07-28 17:55:54 +0100

    Another unknown resource

--- source/unknown.c
+++ source/unknown.c
@@ -3,9 +3,17 @@
 #include "unknown.h"
 
 int unknownResourceCreate(const char *name) {
-	syscall(557, name);
+	syscall(538, name);
 }
 
 int unknownResourceDestroy(int unknownResource) {
+	syscall(539, unknownResource);
+}
+
+int unknownResource2Create(const char *name) {
+	syscall(557, name);
+}
+
+int unknownResource2Destroy(int unknownResource) {
 	syscall(558, unknownResource);
 }
