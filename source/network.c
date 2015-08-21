c11a9794bfe6396f60705642ae9aa76046f456b4
CTurt <ertl.chris@gmail.com>
2015-08-21 15:34:34 +0100

    Server side networking

--- source/network.c
+++ source/network.c
@@ -3,9 +3,12 @@
 #include "module.h"
 
 int (*sceNetSocket)(const char *, int, int, int);
+int (*sceNetSocketClose)(int);
 int (*sceNetConnect)(int, struct sockaddr_in *, int);
 int (*sceNetSend)(int, const void *, size_t, int);
-int (*sceNetSocketClose)(int);
+int (*sceNetBind)(int, struct sockaddr_in *, int);
+int (*sceNetAccept)(int, struct sockaddr_in *, int *);
+int (*sceNetRecv)(int, void *, size_t, int);
 
 void initNetwork(void) {
 	int libNet;
@@ -13,7 +16,10 @@ void initNetwork(void) {
 	loadModule(libNetName, &libNet);
 	
 	RESOLVE(libNet, sceNetSocket);
+	RESOLVE(libNet, sceNetSocketClose);
 	RESOLVE(libNet, sceNetConnect);
 	RESOLVE(libNet, sceNetSend);
-	RESOLVE(libNet, sceNetSocketClose);
+	RESOLVE(libNet, sceNetBind);
+	RESOLVE(libNet, sceNetAccept);
+	RESOLVE(libNet, sceNetRecv);
 }
