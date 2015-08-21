9584ff0b9aa31b25d3726941db88f0043eb049ae
CTurt <ertl.chris@gmail.com>
2015-08-21 22:33:52 +0100

    Spare a register

--- source/syscall.s
+++ source/syscall.s
@@ -3,12 +3,10 @@
 
 .global syscall
 syscall:
-	push	r14
 	push	r15
-	mov     r14, 0x9263FFFF8
-	mov     r15, [r14]
+	mov     r15, 0x9263FFFF8
+	mov     r15, [r15]
 	xor     rax, rax
 	call    r15
 	pop		r15
-	pop		r14
 	ret
