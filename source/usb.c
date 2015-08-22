b0f39898447ad95518b6a39ab7acfae1d49056be
CTurt <cturt@users.noreply.github.com>
2015-08-22 16:14:56 +0100

    Fix USB

--- source/usb.c
+++ source/usb.c
@@ -2,6 +2,12 @@
 
 #include "usb.h"
 
+int (*sceUsbdInit)(void);
+void (*sceUsbdExit)(void);
+ssize_t (*sceUsbdGetDeviceList)(libusb_device ***list);
+void (*sceUsbdFreeDeviceList)(libusb_device **list, int unrefDevices);
+int (*sceUsbdGetDeviceDescriptor)(libusb_device *device, libusb_device_descriptor *desc);
+
 void initUsb(void) {
 	int libUsb;
 	char libUsbName[] = "libSceUsbd.sprx";
