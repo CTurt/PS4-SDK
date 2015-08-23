473c594012317daca1ede333ad1b20ef0bc41705
CTurt <cturt@users.noreply.github.com>
2015-08-23 14:18:54 +0100

    FreeBSD file syscalls

--- include/file.h
+++ include/file.h
@@ -1,3 +1,8 @@
 #pragma once
 
+ssize_t read(int fd, void *buf, size_t nbyte);
+int open(char *path, int flags, int mode);
+int close(int fd);
+int getdents(int fd, char *buf, size_t count);
+
 int getSandboxDirectory(char *destination, int *length);
--- source/file.c
+++ source/file.c
@@ -2,6 +2,22 @@
 
 #include "file.h"
 
+ssize_t read(int fd, void *buf, size_t nbyte) {
+	return syscall(3, fd, buf, nbyte);
+}
+
+int open(char *path, int flags, int mode) {
+	return syscall(5, path, flags, mode);
+}
+
+int close(int fd) {
+	return syscall(6, fd);
+}
+
+int getdents(int fd, char *buf, size_t count) {
+	return syscall(272, fd, buf, count);
+}
+
 int getSandboxDirectory(char *destination, int *length) {
 	return syscall(602, 0, destination, length);
 }
