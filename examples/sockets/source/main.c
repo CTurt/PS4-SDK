f28af2ea0707c9a119f061514e868685527f227b
Sergi Granell <xerpi.g.12@gmail.com>
2015-08-23 16:52:52 +0200

    Update network stuff Add -mcmodel=large

--- examples/sockets/source/main.c
+++ examples/sockets/source/main.c
@@ -14,31 +14,31 @@ int _main(void) {
 	// Pass address of a syscall gadget in rcx
 	register f rcx asm("rcx");
 	directSyscall = rcx;
-	
-	
+
+
 	// Init and resolve libraries
 	initLibc();
 	initNetwork();
-	
-	
+
+
 	// Connect to server and send message
 	char socketName[] = "debug";
-	
+
 	struct sockaddr_in server;
-	
-	server.sin_family = htons(AF_INET);
+
+	server.sin_family = sceNetHtons(AF_INET);
 	server.sin_addr.s_addr = IP(192, 168, 0, 4);
-	server.sin_port = htons(9023);
+	server.sin_port = sceNetHtons(9023);
 	memset(server.sin_zero, 0, sizeof(server.sin_zero));
-	
+
 	int sock = sceNetSocket(socketName, AF_INET, SOCK_STREAM, 0);
 	sceNetConnect(sock, (struct sockaddr *)&server, sizeof(server));
-	
+
 	debug(sock, "PID: %d", syscall(20));
-	
+
 	sceNetSocketClose(sock);
-	
-	
+
+
 	// Return to browser
 	return 0;
 }
