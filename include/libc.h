cae7e961cc5e06043ff68d589e47531981e6385f
Sergi Granell <xerpi.g.12@gmail.com>
2015-08-23 15:24:16 +0200

    Add more libc functions

--- include/libc.h
+++ include/libc.h
@@ -6,5 +6,9 @@ extern void *(*memset)(void *destination, int value, size_t num);
 extern void *(*memcpy)(void *destination, const void *source, size_t num);
 extern char *(*strcpy)(char *destination, const char *source);
 extern int (*sprintf)(char *str, const char *format, ...);
+extern int (*sscanf)(const char *str, const char *format, ...);
+extern char *(*strchr)(const char *s, int c);
+extern char *(*strrchr)(const char *s, int c);
+
 
 void initLibc(void);
--- source/libc.c
+++ source/libc.c
@@ -6,13 +6,20 @@ void *(*memset)(void *destination, int value, size_t num);
 void *(*memcpy)(void *destination, const void *source, size_t num);
 char *(*strcpy)(char *destination, const char *source);
 int (*sprintf)(char *str, const char *format, ...);
+int (*sscanf)(const char *str, const char *format, ...);
+char *(*strchr)(const char *s, int c);
+char *(*strrchr)(const char *s, int c);
+
 
 void initLibc(void) {
 	int libc;
 	loadModule("libSceLibcInternal.sprx", &libc);
-	
+
 	RESOLVE(libc, memset);
 	RESOLVE(libc, memcpy);
 	RESOLVE(libc, strcpy);
 	RESOLVE(libc, sprintf);
+	RESOLVE(libc, sscanf);
+	RESOLVE(libc, strchr);
+	RESOLVE(libc, strrchr);
 }
