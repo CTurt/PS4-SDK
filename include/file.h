ffe772f5f9668686aeb52e85e4dee649b711e7fd
Sergi Granell <xerpi.g.12@gmail.com>
2015-08-23 17:52:53 +0200

    Add const to open syscall

--- include/file.h
+++ include/file.h
@@ -8,7 +8,7 @@ struct stat {
 };
 
 ssize_t read(int fd, void *buf, size_t nbyte);
-int open(char *path, int flags, int mode);
+int open(const char *path, int flags, int mode);
 int close(int fd);
 int fstat(int fd, struct stat *sb);
 int getdents(int fd, char *buf, size_t count);
