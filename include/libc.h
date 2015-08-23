c8e588ec9e6b9c97de3337c9e3e0e5879160fad3
Sergi Granell <xerpi.g.12@gmail.com>
2015-08-23 17:03:30 +0200

    Add strcmp, strncmp

--- include/libc.h
+++ include/libc.h
@@ -8,6 +8,8 @@ extern void *(*memset)(void *destination, int value, size_t num);
 extern void *(*memcpy)(void *destination, const void *source, size_t num);
 extern char *(*strcpy)(char *destination, const char *source);
 extern size_t (*strlen)(const char *s);
+extern int (*strcmp)(const char *s1, const char *s2);
+extern int (*strncmp)(const char *s1, const char *s2, size_t n);
 extern int (*sprintf)(char *str, const char *format, ...);
 extern int (*snprintf)(char *str, size_t size, const char *format, ...);
 extern int (*sscanf)(const char *str, const char *format, ...);
