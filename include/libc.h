dd53e047eabfd3234b0a1554983c309629003802
Sergi Granell <xerpi.g.12@gmail.com>
2015-08-23 15:50:53 +0200

    Add strlen to libc

--- include/libc.h
+++ include/libc.h
@@ -5,6 +5,7 @@
 extern void *(*memset)(void *destination, int value, size_t num);
 extern void *(*memcpy)(void *destination, const void *source, size_t num);
 extern char *(*strcpy)(char *destination, const char *source);
+extern size_t (*strlen)(const char *s);
 extern int (*sprintf)(char *str, const char *format, ...);
 extern int (*sscanf)(const char *str, const char *format, ...);
 extern char *(*strchr)(const char *s, int c);
