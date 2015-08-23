fc77ad12c15c9dcec686adb9db2f7139a2b76907
Sergi Granell <xerpi.g.12@gmail.com>
2015-08-23 15:35:11 +0200

    Add pthread

---
+++ include/pthread.h
@@ -0,0 +1,22 @@
+#pragma once
+
+typedef void *ScePthread;
+typedef void *ScePthreadAttr;
+
+typedef void *ScePthreadMutex;
+typedef void *ScePthreadMutexattr;
+
+extern int (*scePthreadCreate)(ScePthread *thread, const ScePthreadAttr *attr, void *(*entry)(void *), void *arg, const char *name);
+extern void (*scePthreadExit)(void *value);
+extern int (*scePthreadDetach)(ScePthread thread);
+extern int (*scePthreadJoin)(ScePthread thread, void **value_ptr);
+extern void (*scePthreadYield)(void);
+
+extern int (*scePthreadMutexInit)(ScePthreadMutex *mutex, const ScePthreadMutexattr *attr, const char *name);
+extern int (*scePthreadMutexDestroy)(ScePthreadMutex *mutex);
+extern int (*scePthreadMutexLock)(ScePthreadMutex *mutex);
+extern int (*scePthreadMutexTrylock)(ScePthreadMutex *mutex);
+extern int (*scePthreadMutexTimedlock)(ScePthreadMutex *mutex, SceKernelUseconds usec);
+extern int (*scePthreadMutexUnlock)(ScePthreadMutex *mutex);
+
+void initPthread(void);
