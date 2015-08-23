ac4a0128c671bd2400f995db6c7b00b311ee6810
Sergi Granell <xerpi.g.12@gmail.com>
2015-08-23 15:57:29 +0200

    Add struct sockaddr, htonl

--- source/network.c
+++ source/network.c
@@ -4,17 +4,17 @@
 
 int (*sceNetSocket)(const char *, int, int, int);
 int (*sceNetSocketClose)(int);
-int (*sceNetConnect)(int, struct sockaddr_in *, int);
+int (*sceNetConnect)(int, struct sockaddr *, int);
 int (*sceNetSend)(int, const void *, size_t, int);
-int (*sceNetBind)(int, struct sockaddr_in *, int);
+int (*sceNetBind)(int, struct sockaddr *, int);
 int (*sceNetListen)(int, int);
-int (*sceNetAccept)(int, struct sockaddr_in *, int *);
+int (*sceNetAccept)(int, struct sockaddr *, int *);
 int (*sceNetRecv)(int, void *, size_t, int);
 
 void initNetwork(void) {
 	int libNet;
 	loadModule("libSceNet.sprx", &libNet);
-	
+
 	RESOLVE(libNet, sceNetSocket);
 	RESOLVE(libNet, sceNetSocketClose);
 	RESOLVE(libNet, sceNetConnect);
