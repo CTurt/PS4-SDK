eaeb6397d259dcee7e1842628f5d0eca8d0bcc4f
Sergi Granell <xerpi.g.12@gmail.com>
2015-08-23 15:50:53 +0200

    Add strlen to libc

--- source/libc.c
+++ source/libc.c
@@ -5,6 +5,7 @@
 void *(*memset)(void *destination, int value, size_t num);
 void *(*memcpy)(void *destination, const void *source, size_t num);
 char *(*strcpy)(char *destination, const char *source);
+size_t (*strlen)(const char *s);
 int (*sprintf)(char *str, const char *format, ...);
 int (*sscanf)(const char *str, const char *format, ...);
 char *(*strchr)(const char *s, int c);
@@ -18,6 +19,7 @@ void initLibc(void) {
 	RESOLVE(libc, memset);
 	RESOLVE(libc, memcpy);
 	RESOLVE(libc, strcpy);
+	RESOLVE(libc, strlen);
 	RESOLVE(libc, sprintf);
 	RESOLVE(libc, sscanf);
 	RESOLVE(libc, strchr);
