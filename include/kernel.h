6cced88e3b4399aa26871d0f2022f6d2f1bfc797
CTurt <cturt@users.noreply.github.com>
2015-08-25 14:28:55 +0100

    Kernel direct memory functions

--- include/kernel.h
+++ include/kernel.h
@@ -4,6 +4,9 @@
 
 typedef struct timeval SceKernelTimeval;
 
+extern int (*sceKernelAllocateDirectMemory)(off_t searchStart, off_t searchEnd, size_t length, size_t alignment, int type, off_t *physicalAddressDestination);
+extern int (*sceKernelMapDirectMemory)(void **addr, size_t length, int protection, int flags, off_t start, size_t alignment);
+
 extern unsigned int (*sceKernelSleep)(unsigned int seconds);
 extern int (*sceKernelUsleep)(unsigned int microseconds);
 extern int (*sceKernelGettimeofday)(SceKernelTimeval *tp);
--- source/kernel.c
+++ source/kernel.c
@@ -2,17 +2,22 @@
 
 #include "kernel.h"
 
+int (*sceKernelAllocateDirectMemory)(off_t searchStart, off_t searchEnd, size_t length, size_t alignment, int type, off_t *physicalAddressDestination);
+int (*sceKernelMapDirectMemory)(void **addr, size_t length, int protection, int flags, off_t start, size_t alignment);
+
 unsigned int (*sceKernelSleep)(unsigned int seconds);
 int (*sceKernelUsleep)(unsigned int microseconds);
 int (*sceKernelGettimeofday)(SceKernelTimeval *tp);
 uint64_t (*sceKernelGetProcessTime)(void);
 int (*sceKernelGetCurrentCpu)(void);
 
-void initKernel(void)
-{
+void initKernel(void) {
 	int libkernel;
 	loadModule("libkernel.sprx", &libkernel);
-
+	
+	RESOLVE(libkernel, sceKernelAllocateDirectMemory);
+	RESOLVE(libkernel, sceKernelMapDirectMemory);
+	
 	RESOLVE(libkernel, sceKernelSleep);
 	RESOLVE(libkernel, sceKernelUsleep);
 	RESOLVE(libkernel, sceKernelGettimeofday);
