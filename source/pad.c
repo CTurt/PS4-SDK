c95f858c6773292897bdff801049d4a68d38e992
CTurt <ertl.chris@gmail.com>
2015-08-26 19:18:48 +0100

    Update pad example

--- source/pad.c
+++ source/pad.c
@@ -6,6 +6,7 @@ int (*scePadInit)(void);
 int (*scePadOpen)(int userID, int, int, void *);
 int (*scePadClose)(int handle);
 int (*scePadRead)(int handle, void *data, int count);
+int (*scePadReadState)(int handle, void *data);
 
 void initPad(void) {
 	int libPad;
@@ -15,6 +16,7 @@ void initPad(void) {
 	RESOLVE(libPad, scePadOpen);
 	RESOLVE(libPad, scePadClose);
 	RESOLVE(libPad, scePadRead);
+	RESOLVE(libPad, scePadReadState);
 	
 	scePadInit();
 }
