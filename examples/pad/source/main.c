40935ca49e4dba58a8c6cbe96e4e6e026b411bf0
CTurt <cturt@users.noreply.github.com>
2015-08-26 19:18:48 +0100

    Update pad example

--- examples/pad/source/main.c
+++ examples/pad/source/main.c
@@ -1,10 +1,29 @@
+// Can't seem to get data to change
+
 #include "ps4.h"
 
 unsigned char data[512];
 
 int _main(void) {
 	// Init and resolve libraries
+	initLibc();
+	initNetwork();
 	initPad();
+	
+	
+	// Connect to debug server
+	char socketName[] = "debug";
+	
+	struct sockaddr_in server;
+	
+	server.sin_len = sizeof(server);
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = IP(192, 168, 0, 4);
+	server.sin_port = sceNetHtons(9023);
+	memset(server.sin_zero, 0, sizeof(server.sin_zero));
+	
+	int sock = sceNetSocket(socketName, AF_INET, SOCK_STREAM, 0);
+	sceNetConnect(sock, (struct sockaddr *)&server, sizeof(server));
 	
 	
 	// Browser /user/home/ to see all user IDs (in hex)
@@ -12,11 +31,15 @@ int _main(void) {
 	
 	int pad = scePadOpen(userID, 0, 0, NULL);
 	
-	scePadRead(pad, data, 1);
-	
-	scePadClose(pad);
+	while(1) {
+		scePadReadState(pad, data);
+		sceNetSend(sock, data, 0x60, 0);
+	}
 	
 	
 	// Return to browser
+	scePadClose(pad);
+	sceNetSocketClose(sock);
+	
 	return 0;
 }
--- include/pad.h
+++ include/pad.h
@@ -4,5 +4,6 @@ extern int (*scePadInit)(void);
 extern int (*scePadOpen)(int userID, int, int, void *);
 extern int (*scePadClose)(int handle);
 extern int (*scePadRead)(int handle, void *data, int count);
+extern int (*scePadReadState)(int handle, void *data);
 
 void initPad(void);
