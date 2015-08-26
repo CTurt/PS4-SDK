07d99c428f3f042a174a28812a8903e0fb815908
CTurt <cturt@users.noreply.github.com>
2015-08-26 17:54:40 +0100

    pad init

--- source/pad.c
+++ source/pad.c
@@ -2,7 +2,13 @@
 
 #include "pad.h"
 
+int (*scePadInit)(void);
+
 void initPad(void) {
 	int libPad;
 	loadModule("libScePad.sprx", &libPad);
+	
+	RESOLVE(libPad, scePadInit);
+	
+	scePadInit();
 }
