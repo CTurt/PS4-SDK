9aa41501d85c023b86901bbf5af9dd953e4ca579
CTurt <cturt@users.noreply.github.com>
2015-08-26 18:08:11 +0100

    More pad functions

--- source/pad.c
+++ source/pad.c
@@ -3,12 +3,16 @@
 #include "pad.h"
 
 int (*scePadInit)(void);
+int (*scePadOpen)(int, int, int, void *);
+int (*scePadRead)(int handle, void *data, int count);
 
 void initPad(void) {
 	int libPad;
 	loadModule("libScePad.sprx", &libPad);
 	
 	RESOLVE(libPad, scePadInit);
+	RESOLVE(libPad, scePadOpen);
+	RESOLVE(libPad, scePadRead);
 	
 	scePadInit();
 }
