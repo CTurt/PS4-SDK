a135357d4392cc7b4db46f9b95d76e902e2968e0
CTurt <ertl.chris@gmail.com>
2015-08-26 18:08:11 +0100

    More pad functions

--- include/pad.h
+++ include/pad.h
@@ -1,5 +1,7 @@
 #pragma once
 
 extern int (*scePadInit)(void);
+extern int (*scePadOpen)(int, int, int, void *);
+extern int (*scePadRead)(int handle, void *data, int count);
 
 void initPad(void);
