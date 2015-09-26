753f3e649899565e54b3a2f32e8b3eb6047ff45f
CTurt <cturt@users.noreply.github.com>
2015-09-26 19:51:54 +0100

    Fix SYSCALL macro

--- libPS4/include/syscall.h
+++ libPS4/include/syscall.h
@@ -2,7 +2,7 @@
 
 #define asm __asm__
 
-#define SYSCALL(name, number) asm(".intel_syntax"); asm(#name ":"); asm("\tmovq rax, " #number); asm("\tjmp rawSyscall")
+#define SYSCALL(name, number) asm(".intel_syntax noprefix\n" ".global " #name "\n" #name ":\n" "\tmovq rax, " #number "\n\tjmp rawSyscall\n")
 
 typedef unsigned long (*f)();
 #define directSyscall (*(f *)0x9263ffff8)
