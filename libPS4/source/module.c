f464aac50677b0ba9bb2704aacc123fa18c5d597
CTurt <CTurt@users.noreply.github.com>
2015-09-27 12:43:02 +0100

    Self containing getModuleInfo size

--- libPS4/source/module.c
+++ libPS4/source/module.c
@@ -7,7 +7,11 @@ int (*sceSysmoduleLoadModule)(int id);
 
 SYSCALL(getFunctionAddressByName, 591);
 SYSCALL(getLoadedModules, 592);
-SYSCALL(getModuleInfo, 593);
+
+int getModuleInfo(int loadedModuleID, struct moduleInfo *destination) {
+	destination->size = sizeof(*destination);
+	return syscall(593, loadedModuleID, destination);
+}
 
 int loadModule(const char *name, int *idDestination) {
 	return syscall(594, name, 0, idDestination, 0);
