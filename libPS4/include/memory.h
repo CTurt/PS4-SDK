a02952abac1ba1f21471b7bc9f15dea6ffd057da
CTurt <cturt@users.noreply.github.com>
2015-09-27 12:47:40 +0100

    mmap

--- libPS4/include/memory.h
+++ libPS4/include/memory.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#define MAP_SHARED 1
+#define MAP_PRIVATE 2
+#define MAP_TYPE 0xf
+#define MAP_FIXED 0x10
+#define MAP_ANONYMOUS 0x1000
+
 struct memoryRegionInfo {
 	void *base; // 0x0
 	void *end; // 0x8
@@ -12,6 +18,9 @@ struct otherMemoryRegionInfo {
 	char unknown[0xa]; // 0x16
 	char name[32]; // 0x20
 };
+
+void *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset);
+int munmap(void *addr, size_t len);
 
 int getMemoryInfo(void *address, struct memoryRegionInfo *destination);
 int getOtherMemoryInfo(void *address, int nextMatchIfUnmapped, struct otherMemoryRegionInfo *destination);
--- libPS4/source/memory.c
+++ libPS4/source/memory.c
@@ -2,5 +2,8 @@
 
 #include "memory.h"
 
+SYSCALL(mmap, 477);
+SYSCALL(munmap, 73);
+
 SYSCALL(getMemoryInfo, 547);
 SYSCALL(getOtherMemoryInfo, 572);
