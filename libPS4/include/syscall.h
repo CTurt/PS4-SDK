92e42b3f47be982e04a654183b374c7a2664957c
CTurt <ertl.chris@gmail.com>
2015-10-11 09:09:14 +0100

    Fix relocation error

--- libPS4/include/syscall.h
+++ libPS4/include/syscall.h
@@ -2,7 +2,18 @@
 
 #define asm __asm__
 
-#define SYSCALL(name, number) asm(".intel_syntax noprefix\n" ".global " #name "\n" #name ":\n" "\tmovq rax, " #number "\n\tjmp rawSyscall\n")
+//static inline unsigned long rSyscall() {
+	__asm__(".intel_syntax noprefix\n\
+rSyscall:\n\
+	push	r15\n\
+	movq    r15, 0x9263FFFF8\n\
+	mov     r15, [r15]\n\
+	call    r15\n\
+	pop		r15\n\
+	ret");
+//}
+
+#define SYSCALL(name, number) asm(".intel_syntax noprefix\n" ".global " #name "\n" #name ":\n" "\tmovq rax, " #number "\n\tcall rSyscall\n\tret\n")
 
 typedef unsigned long (*f)();
 #define directSyscall (*(f *)0x9263ffff8)
