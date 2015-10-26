bff22c07eaf0cf6cb011d31bd1266976d112c394
CTurt <cturt@users.noreply.github.com>
2015-10-26 13:50:14 +0000

    stdint types

--- libPS4/include/types.h
+++ libPS4/include/types.h
@@ -1,20 +1,12 @@
 #pragma once
 
+#include <stdint.h>
+
 #ifndef NULL
 #define NULL 0
 #endif
 
 #define BIT(n) (1 << (n))
-
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
-
-typedef char int8_t;
-typedef short int16_t;
-typedef int int32_t;
-typedef long long int64_t;
 
 typedef unsigned int size_t;
 typedef signed int ssize_t;
@@ -68,9 +60,9 @@ typedef volatile s16 vs16;
 typedef volatile s32 vs32;
 typedef volatile s64 vs64;
 
-typedef int ptrdiff_t;
-typedef unsigned int *uintptr_t;
-typedef int *intptr_t;
+//typedef int ptrdiff_t;
+//typedef unsigned int *uintptr_t;
+//typedef int *intptr_t;
 
 /* POSIX types */
 
