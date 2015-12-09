509b2741bebede98f87a114961bc2e6914f5d156
CTurt <CTurt@users.noreply.github.com>
2015-12-09 22:53:28 +0000

    Update memory.h

--- libPS4/include/memory.h
+++ libPS4/include/memory.h
@@ -11,12 +11,22 @@
 #define PROT_GPU_READ 16
 #define PROT_GPU_WRITE 32
 
+#define PROT_READ PROT_CPU_READ
+#define PROT_WRITE PROT_CPU_WRITE
+#define PROT_EXEC PROT_CPU_EXEC
+
 #define MAP_SHARED 1
 #define MAP_PRIVATE 2
 #define MAP_TYPE 0xf
 #define MAP_FIXED 0x10
 #define MAP_ANONYMOUS 0x1000
 #define MAP_32BIT 0x80000
+
+#define MAP_FAILED (void *)-1
+
+#define	MS_SYNC 0x0000
+#define MS_ASYNC 0x0001
+#define MS_INVALIDATE 0x0002
 
 struct memoryRegionInfo {
 	void *base; // 0x0
@@ -33,6 +43,7 @@ struct otherMemoryRegionInfo {
 
 void *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset);
 int munmap(void *addr, size_t len);
+int msync(void *addr, size_t len, int flags);
 
 int getMemoryInfo(void *address, struct memoryRegionInfo *destination);
 int getOtherMemoryInfo(void *address, int nextMatchIfUnmapped, struct otherMemoryRegionInfo *destination);
