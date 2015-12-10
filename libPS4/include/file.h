9d02e6ed7f596cf8acc2f22464756a666c8ea9d7
CTurt <CTurt@users.noreply.github.com>
2015-12-10 23:13:29 +0000

    Update file.h

--- libPS4/include/file.h
+++ libPS4/include/file.h
@@ -113,12 +113,12 @@ ssize_t write(int fd, const void *buf, size_t count);
 int open(const char *path, int flags, int mode);
 int close(int fd);
 int unlink(const char *pathname);
+int fchmod(int fd, int mode);
 int rename(const char *oldpath, const char *newpath);
 int mkdir(const char *pathname, mode_t mode);
 int rmdir(const char *path);
 int stat(const char *path, struct stat *sb);
 int fstat(int fd, struct stat *sb);
-int fchmod(int fd, int mode);
 int getdents(int fd, char *buf, size_t count);
 
 int getSandboxDirectory(char *destination, int *length);
