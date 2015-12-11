8439351939db7469b4207f730f15b86caac84b40
CTurt <CTurt@users.noreply.github.com>
2015-12-11 19:14:54 +0000

    Update file.c

--- libPS4/source/file.c
+++ libPS4/source/file.c
@@ -7,7 +7,7 @@ SYSCALL(write, 4);
 SYSCALL(open, 5);
 SYSCALL(close, 5);
 SYSCALL(unlink, 10);
-SYSCALL(fchmod, 24);
+SYSCALL(fchmod, 124);
 SYSCALL(rename, 128);
 SYSCALL(mkdir, 136);
 SYSCALL(rmdir, 137);
