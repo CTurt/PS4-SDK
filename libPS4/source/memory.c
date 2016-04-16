2a71555f5792c974b0eb93f60de68c3bf2b86070
CTurt <CTurt@users.noreply.github.com>
2016-04-16 11:17:27 +0100

    Update memory.c

--- libPS4/source/memory.c
+++ libPS4/source/memory.c
@@ -6,6 +6,8 @@ SYSCALL(mmap, 477);
 SYSCALL(munmap, 73);
 SYSCALL(mprotect, 74);
 SYSCALL(msync, 65);
+SYSCALL(mlock, 203);
+SYSCALL(munlock, 204);
 
 SYSCALL(getMemoryInfo, 547);
 SYSCALL(getOtherMemoryInfo, 572);
