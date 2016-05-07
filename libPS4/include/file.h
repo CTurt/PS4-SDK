224a9e20a26b6fa0d1b391a0a9d1928bf8192926
CTurt <cturt@users.noreply.github.com>
2016-05-07 21:50:46 +0100

    File header fixes

--- libPS4/include/file.h
+++ libPS4/include/file.h
@@ -7,76 +7,17 @@ enum {
 	SEEK_CUR,
 	SEEK_END
 };
-/* Grabbed from FreeBSD/sys/sys/stat.h */
 
 #define O_RDONLY  0x0000
 #define O_WRONLY  0x0001
 #define O_RDWR    0x0002
 #define O_ACCMODE 0x0003
 
-#define O_BINARY    0x0004
-#define O_TEXT      0x0008
-#define O_NOINHERIT 0x0080
-
-#define O_CREAT    0x0100
-#define O_EXCL     0x0200
-#define O_NOCTTY   0x0400
-#define O_TRUNC    0x0800
-#define O_APPEND   0x1000
-#define O_NONBLOCK 0x2000
-
-#define DT_UNKNOWN 0
-#define DT_FIFO    1
-#define DT_CHR     2
-#define DT_DIR     4
-#define DT_BLK     6
-#define DT_REG     8
-#define DT_LNK     10
-#define DT_SOCK    12
-#define DT_WHT     14
-
-#define	S_ISUID	0004000			/* set user id on execution */
-#define	S_ISGID	0002000			/* set group id on execution */
-#define	S_ISTXT	0001000			/* sticky bit */
-
-#define	S_IRWXU	0000700			/* RWX mask for owner */
-#define	S_IRUSR	0000400			/* R for owner */
-#define	S_IWUSR	0000200			/* W for owner */
-#define	S_IXUSR	0000100			/* X for owner */
-
-#define	S_IREAD		S_IRUSR
-#define	S_IWRITE	S_IWUSR
-#define	S_IEXEC		S_IXUSR
-
-#define	S_IRWXG	0000070			/* RWX mask for group */
-#define	S_IRGRP	0000040			/* R for group */
-#define	S_IWGRP	0000020			/* W for group */
-#define	S_IXGRP	0000010			/* X for group */
-
-#define	S_IRWXO	0000007			/* RWX mask for other */
-#define	S_IROTH	0000004			/* R for other */
-#define	S_IWOTH	0000002			/* W for other */
-#define	S_IXOTH	0000001			/* X for other */
-
-#define	S_IFMT	 0170000		/* type of file mask */
-#define	S_IFIFO	 0010000		/* named pipe (fifo) */
-#define	S_IFCHR	 0020000		/* character special */
-#define	S_IFDIR	 0040000		/* directory */
-#define	S_IFBLK	 0060000		/* block special */
-#define	S_IFREG	 0100000		/* regular */
-#define	S_IFLNK	 0120000		/* symbolic link */
-#define	S_IFSOCK 0140000		/* socket */
-#define	S_ISVTX	 0001000		/* save swapped text even after use */
-#define	S_IFWHT  0160000		/* whiteout */
-
-#define	S_ISDIR(m)	(((m) & 0170000) == 0040000)	/* directory */
-#define	S_ISCHR(m)	(((m) & 0170000) == 0020000)	/* char special */
-#define	S_ISBLK(m)	(((m) & 0170000) == 0060000)	/* block special */
-#define	S_ISREG(m)	(((m) & 0170000) == 0100000)	/* regular file */
-#define	S_ISFIFO(m)	(((m) & 0170000) == 0010000)	/* fifo or socket */
-#define	S_ISLNK(m)	(((m) & 0170000) == 0120000)	/* symbolic link */
-#define	S_ISSOCK(m)	(((m) & 0170000) == 0140000)	/* socket */
-#define	S_ISWHT(m)	(((m) & 0170000) == 0160000)	/* whiteout */
+#define	O_NONBLOCK	0x0004		/* no delay */
+#define	O_APPEND	0x0008		/* set append mode */
+#define	O_CREAT		0x0200		/* create if nonexistent */
+#define	O_TRUNC		0x0400		/* truncate to zero length */
+#define	O_EXCL		0x0800		/* error if already exists */
 
 struct stat {
 	__dev_t   st_dev;		/* inode's device */
