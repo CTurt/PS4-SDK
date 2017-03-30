4854bb6036c8c644c508f23673060e30fc8416bb
CTurt <CTurt@users.noreply.github.com>
2017-03-30 21:56:10 +0100

    Update libc.c

--- libPS4/source/libc.c
+++ libPS4/source/libc.c
@@ -53,6 +53,7 @@ size_t (*fwrite)(const void * ptr, size_t size, size_t count, FILE *stream );
 int (*fseek)(FILE *stream, long int offset, int origin);
 long int(*ftell)(FILE *stream);
 int (*fclose)(FILE *stream);
+int (*fprintf)(FILE *stream, const char *format, ...);
 
 void initLibc(void) {
 	int libc = sceKernelLoadStartModule("libSceLibcInternal.sprx", 0, NULL, 0, 0, 0);
@@ -108,4 +109,5 @@ void initLibc(void) {
 	RESOLVE(libc, fseek);
 	RESOLVE(libc, ftell);
 	RESOLVE(libc, fclose);
+	RESOLVE(libc, fprintf);
 }
